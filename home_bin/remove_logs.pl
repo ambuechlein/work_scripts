dc2c6d0195fd97cffe69cfd9673504c567debecf
ambuechlein <aaron.buechlein@gmail.com>
2019-03-29 10:30:42 -0400

    edited to be UITS specific

---
+++ home_bin/remove_logs.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+foreach my $file (@ARGV){
+  my $line = `tail -n 1 $file`;
+  chomp $line;
+  if($line eq 'Finished'){
+    if($file =~ /(\S+)\.sh\.o(\d+)/o){
+      my $file2 = $1.'.sh.e'.$2;
+      my $file3 = $1.'.sh.pe'.$2;
+      my $file4 = $1.'.sh.po'.$2;
+      if(-e $file3 and -e $file4){
+        system("rm $file $file2 $file3 $file4");
+      } elsif( -e $file2 ) {
+        system("rm $file $file2");
+      } else {
+        system("rm $file");
+      }
+    } else {
+      system("rm $file");
+    }
+  }
+}
+exit;
